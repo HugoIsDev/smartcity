7337c119e560305d859d64d8e2de1b4e0890a19b
HugoIsDev <hugogonzalezruizjob@gmail.com>
2023-11-17 11:01:19 +0000

    prueba

---
+++ Main.java
@@ -0,0 +1,12 @@
+package smartcity;
+
+import java.io.IOException;
+
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		Window1 w1 = new Window1(); // Main class that opens window 1
+	}
+}
+
